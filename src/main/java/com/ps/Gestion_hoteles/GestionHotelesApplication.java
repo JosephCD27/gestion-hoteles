5c4ea7acbc921540bb369ecf74237103eded8381
JosephCD27 <joseph.camacho2005@gmail.com>
2025-06-18 20:38:49 -0500

    primer commit

---
+++ src/main/java/com/ps/Gestion_hoteles/GestionHotelesApplication.java
@@ -0,0 +1,13 @@
+package com.ps.Gestion_hoteles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHotelesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionHotelesApplication.class, args);
+	}
+
+}
